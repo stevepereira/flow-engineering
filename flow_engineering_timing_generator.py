91c935db0a7813ee1c455389e60788b4bf128ec6
Andrew Davis <abd3721@gmail.com>
2024-05-15 21:27:56 +0200

    Create flow_engineering_timing_generator.py
    A random number generator for the Flow Engineering game

---
+++ flow_engineering_timing_generator.py
@@ -0,0 +1,26 @@
+import random
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+units = [[' hours', .125], [' days', 1], [' weeks', 5]]
+
+print("Work times:")
+total_step_time=0
+total_wait_time=0
+for x in range(8):
+  unit = random.choice(units)
+  step_time = random.choice(numbers)
+  total_step_time += step_time * unit[1]
+  print(str(step_time) + unit[0])
+
+print("\nWait times:")
+for x in range(7):
+  unit = random.choice(units)
+  wait_time = random.choice(numbers)
+  total_wait_time += wait_time * unit[1]
+  print(str(wait_time) + unit[0])
+
+print("\nTotal Step Time: " + f"{round(total_step_time, 1)} days")
+print("\nTotal Wait Time: " + f"{round(total_wait_time, 1)} days")
+total_lead_time = total_step_time + total_wait_time
+print("\nTotal Lead Time: " + f"{round(total_lead_time, 1)} days")
+print("\nStep Time as a % of Lead Time: " +f"{total_step_time/total_lead_time:.0%}")
